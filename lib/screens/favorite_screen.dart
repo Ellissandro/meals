0e343d939e46049efea72c8c865aeb5d15d83d1c
Elissandro Silva <elissandros667@gmail.com>
2022-10-23 16:39:38 -0300

    Meals screen

---
+++ lib/screens/favorite_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:meals/components/meal_item.dart';
+
+import '../models/meal.dart';
+
+class FavoriteScreen extends StatelessWidget {
+  const FavoriteScreen(this.favoriteMeals, {super.key});
+
+  final List<Meal> favoriteMeals;
+
+  @override
+  Widget build(BuildContext context) {
+    if (favoriteMeals.isEmpty) {
+      return const Center(
+        child: Text('Nenhuma refeição foi marcada como favorita!'),
+      );
+    }
+
+    return ListView.builder(
+      itemCount: favoriteMeals.length,
+      itemBuilder: (ctx, index) {
+        return MealItem(favoriteMeals[index]);
+      },
+    );
+  }
+}
